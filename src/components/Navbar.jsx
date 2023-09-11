aec8e9bab8065996d69d633173b7eb812b39ced4
alexjachna <alex.jachna1@gmail.com>
2023-09-11 00:22:42 -0400

    added basic responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import menu from "../assets/menu.png";
 
 export default function Navbar() {
+  const [size, setSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setSize(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerWidth]);
+
   return (
-    <div className="fixed flex justify-evenly items-center w-full h-20 bg-zinc-50 shadow-sm">
+    <div className="fixed flex justify-evenly items-center w-full h-20 bg-zinc-50 shadow-sm z-10">
       <p className="text-zinc-800 font-dmSerif text-3xl border-b border-b-zinc-500">
-        Chell Creates.
+        chell creates.
       </p>
-      <nav className="flex gap-8 text-zinc-800 font-bold">
-        <p>Galleries</p>
-        <p>About</p>
-        <p>Contact</p>
-      </nav>
+      {size >= 1024 ? (
+        <nav className="flex gap-8 text-zinc-800 font-bold">
+          <p>Galleries</p>
+          <p>About</p>
+          <p>Contact</p>
+        </nav>
+      ) : (
+        <img src={menu} alt="hamburger menu" className="w-10" />
+      )}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
+  const [size, setSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setSize(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerWidth]);
+
   return (
     <div className="relative flex w-full h-screen">
       <Navbar />
-      <div className="h-full w-1/2 left-0 bg-zinc-50"></div>
-      <div className="h-full w-1/2 right-0 bg-stone-200"></div>
-      <div className="absolute flex flex-col gap-8 w-full h-full p-60 m-auto left-0 right-0 top-0 bottom-0 ">
-        <p className="text-zinc-400 tracking-[1rem] text-2xl">
+      {size >= 1024 && (
+        <>
+          <div className="h-full w-1/2 left-0 bg-zinc-50"></div>
+          <div className="h-full w-1/2 right-0 bg-stone-200"></div>
+        </>
+      )}
+
+      <div className="absolute flex flex-col items-center lg:items-start gap-8 w-full h-fit px-4 lg:px-48 m-auto left-0 right-0 top-0 bottom-0 ">
+        <p className="text-zinc-400 tracking-[1rem] text-xl lg:text-2xl">
           ALEX JACHNA PRESENTS
         </p>
-        <p className="text-zinc-800 font-dmSerif text-9xl">
-          Chell Creates Media.
+        <p className="text-zinc-800 font-dmSerif text-8xl lg:text-9xl text-center">
+          chell creates media.
         </p>
-        <nav className="flex gap-44">
-          <div className="p-4 w-48">
-            <Link
-              to="/galleries/"
-              className="font-bold text-2xl text-zinc-700 tracking-widest"
-            >
-              Galleries
-            </Link>
-            <p className="text-zinc-500">Visit all my photoshoot galleries.</p>
-          </div>
-          <div className="p-4 w-48">
-            <Link
-              to="/galleries/"
-              className="font-bold text-2xl text-zinc-700 tracking-widest"
-            >
-              About
-            </Link>
-            <p className="text-zinc-500">Learn more about me and what I do.</p>
-          </div>
-          <div className="p-4 w-48">
-            <Link
-              to="/galleries/"
-              className="font-bold text-2xl text-zinc-700 tracking-widest"
-            >
-              Contact
-            </Link>
-            <p className="text-zinc-500">Feel free to get in touch with me.</p>
-          </div>
-        </nav>
+        {size >= 1024 && (
+          <nav className="flex gap-4 lg:gap-44">
+            <div className="p-4 w-48">
+              <Link
+                to="/galleries/"
+                className="font-bold text-2xl text-zinc-700 tracking-widest"
+              >
+                Galleries
+              </Link>
+              <p className="text-zinc-500">
+                Visit all my photoshoot galleries.
+              </p>
+            </div>
+            <div className="p-4 w-48">
+              <Link
+                to="/galleries/"
+                className="font-bold text-2xl text-zinc-700 tracking-widest"
+              >
+                About
+              </Link>
+              <p className="text-zinc-500">
+                Learn more about me and what I do.
+              </p>
+            </div>
+            <div className="p-4 w-48">
+              <Link
+                to="/galleries/"
+                className="font-bold text-2xl text-zinc-700 tracking-widest"
+              >
+                Contact
+              </Link>
+              <p className="text-zinc-500">
+                Feel free to get in touch with me.
+              </p>
+            </div>
+          </nav>
+        )}
         <button className="bg-purple-400 hover:bg-purple-500 transition-all p-4 w-40 text-white rounded-sm shadow-md">
           View Galleries
         </button>
